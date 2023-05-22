d0b23b2172e92f7341f2d08a17dedb779c0cd97a
diegobobasso <diego.marcospaz@gmail.com>
2023-05-22 15:25:08 -0300

    arreglo solo toma números positivos

--- script/compratickets.js
+++ script/compratickets.js
@@ -9,17 +9,23 @@ function TotalPago() {
     if(regex.test(cantidad.value)) {
 
         let cant = parseInt(cantidad.value);
-        let cate = categoria.value;
-        let desc = 0;
         
-        if(cate == "estudiante") desc = 0.20;
-        if(cate == "trainee") desc = 0.5;
-        if(cate == "junior") desc = 0.85;
+        if (cant > 0 ) {
+        
+            let cate = categoria.value;
+            let desc = 0;
+            
+            if(cate == "estudiante") desc = 0.20;
+            if(cate == "trainee") desc = 0.5;
+            if(cate == "junior") desc = 0.85;
+        
+            let total = 200 * cant * desc;
     
-        let total = 200 * cant * desc;
-
-        pago.innerHTML = total.toString();
-    }
+            pago.innerHTML = total.toString();
+        
+        }
+        
+        }
     else {
         pago.innerHTML = "0";    
     }
